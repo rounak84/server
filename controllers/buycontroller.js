ec08b87960674f42613f75cc09a8362ba288cb5c
Rounak <rounaknaik23@gmail.com>
2023-04-12 14:58:51 +0530

    Changed getproducts to get request

--- controllers/buycontroller.js
+++ controllers/buycontroller.js
@@ -9,3 +9,13 @@ export const getProducts = async (req, res) => {
     res.status(404).json({ message: error.message });
   }
 };
+
+export const buyProduct = async (req, res) => {
+  try {
+    const prods = await product.find({});
+    // console.log(AdminItem);
+    res.status(200).json(prods);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
