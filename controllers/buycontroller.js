f24ca8fc04d9765ae254df91508ff871be6b9545
Rounak <rounaknaik23@gmail.com>
2023-04-12 14:42:28 +0530

    Added sell auth checking

---
+++ controllers/buycontroller.js
@@ -0,0 +1,9 @@
+// export const getAdminItems = async (req, res) => {
+//   try {
+//     const adminItem = await AdminItem.find({});
+//     // console.log(AdminItem);
+//     res.status(200).json(adminItem);
+//   } catch (error) {
+//     res.status(404).json({ message: error.message });
+//   }
+// };
